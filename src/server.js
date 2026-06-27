bcecea43daf1d8e3ddbfe046eaa07ff070988f41
Nishant Tiwari <nishant7tiwari19@gmail.com>
2026-06-27 15:37:40 +0530

    Replace console.log with centralized logger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import http from "http";
-
 import app from "./app.js";
-
 import { initializeSocket } from "./socket/socket.js";
+import logger from "./shared/logger/logger.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,13 +10,13 @@ const server = http.createServer(app);
 initializeSocket(server);
 
 server.listen(PORT, () => {
-    console.log(`
+  logger.info(`
 =========================================
 🚀 Aurelia Backend Started Successfully
 =========================================
-Server : http://localhost:${PORT}
+Server      : http://localhost:${PORT}
 Environment : ${process.env.NODE_ENV || "development"}
-Socket.IO : Enabled
+Socket.IO   : Enabled
 =========================================
 `);
 });
