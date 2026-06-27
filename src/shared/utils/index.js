7eff70e0a3add0c65fc4406cb824b58091790229
Nishant Tiwari <nishant7tiwari19@gmail.com>
2026-06-27 15:34:20 +0530

    Refactor API response utilities

--- src/shared/utils/index.js
+++ src/shared/utils/index.js
@@ -1,15 +1,15 @@
 export { default as asyncHandler } from "./asyncHandler.js";
 
-export * from "./apiResponse.js";
+export { default as ApiResponse } from "./ApiResponse.js";
 
 export * from "./pagination.js";
 
 export { generateSlug, generateUniqueSlug } from "./slugify.js";
-
-export { default as logger } from "./logger.js";
 
 export { default as generateOrderNumber } from "./generateOrderNumber.js";
 
 export * from "./jwt.js";
 
 export * from "./password.js";
+
+
